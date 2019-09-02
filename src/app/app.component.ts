8e857a9007da8f0fadb48fb42586615334f66f2f
cdsalty <soltiscd@gmail.com>
2019-09-01 22:38:24 -0400

    configuration of new app, practiced with ngModel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-root', //pulling our app into html file; scripts injected dynmicaly
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'max-first-app';
+  name = 'Christopher';  // As of app initial setup, it was title = 'my-first-app'
 }
