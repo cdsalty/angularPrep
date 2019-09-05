cd185e4211c29f8da74a49851c656383cb7cd33b
cdsalty <soltiscd@gmail.com>
2019-09-04 23:54:58 -0400

    small updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root', //pulling our app into html file; scripts injected dynmicaly
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styles: [`
+      h3 {
+        color: dodgerblue;
+      }
+  `]
 })
 export class AppComponent {
-  name = 'Christopher';  // As of app initial setup, it was title = 'my-first-app'
+ 
 }
